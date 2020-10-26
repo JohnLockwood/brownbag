7e881e7a83a6fb419b7946112d038ff3de487dde
John Lockwood <john.lockwood1@hitachivantara.com>
2020-10-26 03:51:08 -0400

    Remove wrong comment

--- tests/test_database_connection.py
+++ tests/test_database_connection.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-# Needed to force table creation
 from app.model.models import User
 
 from app.model.database import Base
