100e72ed760af95491a5a9c7ba680f0971443fd6
John Lockwood <John@Codesolid.com>
2020-11-22 14:00:58 -0500

    Allow proxy integration -- hello shows all you need for correct response

--- app/lambda/hello.py
+++ app/lambda/hello.py
@@ -2,8 +2,8 @@ import json
 
 
 def lambda_handler(event, context):
+
     return {
         'statusCode': 200,
-        'event': event,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(event)
     }
