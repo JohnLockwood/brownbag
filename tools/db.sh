d2cb7d8fd35cd6500ce1190f43203862d542d063
John Lockwood <john.lockwood1@hitachivantara.com>
2020-12-11 16:53:17 -0500

    Create proxy container.  Install docker-compose on ec2, good rework of cfn-init script

--- tools/db.sh
+++
@@ -1,2 +0,0 @@
-aws cloudformation delete-stack --stack-name prod-brownbag
-./scripts/upsert_stack.sh prod-brownbag prod/db.yml --capabilities CAPABILITY_AUTO_EXPAND
---
+++ tools/scripts/ec2.sh
@@ -0,0 +1,12 @@
+stack=brownbag-ec2
+file="dev/ec2.yml"
+
+echo deleting stack if it exists:  "${stack}"
+aws cloudformation delete-stack --stack-name "${stack}"
+
+echo waiting for stack to be deleted
+aws cloudformation wait stack-delete-complete --stack-name "${stack}"
+
+echo creating stack
+./upsert_stack.sh "${stack}" "${file}" --capabilities CAPABILITY_IAM
+
