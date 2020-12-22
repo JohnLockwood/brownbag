18399e8c2be57a73b68e0890d1b0ded055e9a513
John Lockwood <john.lockwood1@hitachivantara.com>
2020-12-22 14:02:57 -0500

    Pretty good volume create logic working. More testing needed.

--- tools/scripts/ec2.sh
+++ tools/scripts/ec2.sh
@@ -1,6 +1,11 @@
 stack=brownbag-ec2
 file="prod/ec2.yml"
+PARAMETERS=""
 
+# count_existing=$(aws ec2 describe-volumes --filters Name=tag:Name,Values=BrownbagEC2Volume | jq '.Volumes | length')
+VolumeId=$(aws ec2 describe-volumes --filters Name=tag:Name,Values=BrownbagEC2Volume | jq -r ".Volumes[0].VolumeId")
+PARAMETERS="--parameters ParameterKey=VolumeId,ParameterValue=$VolumeId"
+echo $PARAMETERS
 echo deleting stack if it exists:  "${stack}"
 aws cloudformation delete-stack --stack-name "${stack}"
 
@@ -8,5 +13,5 @@ echo waiting for stack to be deleted
 aws cloudformation wait stack-delete-complete --stack-name "${stack}"
 
 echo creating stack
-./upsert_stack.sh "${stack}" "${file}" --capabilities CAPABILITY_IAM
+./upsert_stack.sh "${stack}" "${file}" "--capabilities CAPABILITY_IAM $PARAMETERS"
 
